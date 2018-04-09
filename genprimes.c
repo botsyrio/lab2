d8ae9265f413cc4e05a23a414363765d28109229
botsyrio <df1593@nyu.edu>
2018-04-09 17:10:42 -0400

    c

--- genprimes.c
+++ genprimes.c
@@ -12,13 +12,14 @@ int main(int argc, char *argv[]){
 	int thread_count = atoi(argv[2]);
 	//printf("thread_count: %d", thread_count);
 	#pragma omp parallel for
-	for(int i = 2; i<=n; i++)
-		numbers[i] = i;
+	numbers[2]=1;
+	for(int i = 3; i<=n; i+=2)//we can optimize by taking out all the evens besides 2
+		numbers[i] = 1;
 	
 	tstart = omp_get_wtime(); 
 	int i;
 	#pragma omp parallel for num_threads(thread_count) private(i) schedule(dynamic 10)
-	for(i=2; i<((n+1)/2); i++){
+	for(i=3; i<((n+1)/2); i+=2){
 		if(numbers[i] != 0){
 			for(int j=2; j<=n/i; j++){
 				if(numbers[j*i]!=0)
@@ -40,7 +41,7 @@ int main(int argc, char *argv[]){
 	int a = 1;
 	int prevB=2;
 	fprintf(fp, "%d %d %d\n", a, 2, 0);
-    for(int b = 3; b <= n; b++){
+    for(int b = 3; b <= n; b+=2){
 		if(numbers[b]!=0){
 			a++;
 			fprintf(fp,"%d %d %d\n", a, b, b-prevB);
