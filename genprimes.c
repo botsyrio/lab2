5f143ad0e1ad9423881a6a2cf74983b11d9a1b2e
botsyrio <df1593@nyu.edu>
2018-04-09 17:43:01 -0400

    a

--- genprimes.c
+++ genprimes.c
@@ -8,17 +8,17 @@ int main(int argc, char *argv[]){
 	char output[100] = "";
 	
 	int n = atoi(argv[1]);
-	int oddsLength = (n-4)/2;
+	int oddsLength = (n-2)/2;
 	int odds[oddsLength];//if odds[i]=1, this means the number represented by i*2+3 is currently noted as a possible prime.
 	int thread_count = atoi(argv[2]);
 	//printf("thread_count: %d", thread_count);
 	#pragma omp parallel for
-	for(int i = 0; i<oddsLength; i++)//we can optimize by taking out all the evens besides 2
+	for(int i = 1; i<oddsLength; i++)//we can optimize by taking out all the evens besides 2
 		odds[i] = 1;
 	
 	tstart = omp_get_wtime(); 
 	#pragma omp parallel for num_threads(thread_count) schedule(dynamic, 10)
-	for(int i=0; i<(oddsLength); i++){
+	for(int i=1; i<(oddsLength); i++){
 		if(odds[i] != 0){
 			for(int j=3; j*i<oddsLength; j+=2){
 				if(odds[j*i]!=0)
@@ -41,9 +41,9 @@ int main(int argc, char *argv[]){
 	int prevB=2;
 	int b;
 	fprintf(fp, "%d %d %d\n", a, 2, 0);
-    for(int i = 3; i <= n; i+=2){
+    for(int i = 1; i <= oddsLength; i++){
 		if(odds[i]!=0){
-			b=i*2+3;
+			b=i*2+2;
 			a++;
 			fprintf(fp,"%d %d %d\n", a, b, b-prevB);
 			prevB = b;
