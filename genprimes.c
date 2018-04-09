08b7b574e64152e025703efad903c88723647b33
botsyrio <df1593@nyu.edu>
2018-04-09 16:46:37 -0400

    v

--- genprimes.c
+++ genprimes.c
@@ -16,8 +16,9 @@ int main(int argc, char *argv[]){
 		numbers[i] = i;
 	
 	tstart = omp_get_wtime(); 
-	#pragma omp parallel for num_threads(thread_count)
-	for(int i=2; i<((n+1)/2); i++){
+	int i;
+	#pragma omp parallel for num_threads(thread_count) shared(i)
+	for(i=2; i<((n+1)/2); i++){
 		if(numbers[i] != 0){
 			for(int j=2; j<=n/i; j++){
 				if(numbers[j*i]!=0)
